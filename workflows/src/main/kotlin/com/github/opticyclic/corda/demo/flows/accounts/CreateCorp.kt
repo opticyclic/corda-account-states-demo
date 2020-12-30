a37ce4213643af16e60c0c15b332390214043032
opticyclic <opticyclic@gmail.com>
2020-12-30 15:10:40 -0500

    Add a workflow to create our new custom account state

---
+++ workflows/src/main/kotlin/com/github/opticyclic/corda/demo/flows/accounts/CreateCorp.kt
@@ -0,0 +1,84 @@
+package com.github.opticyclic.corda.demo.flows.accounts
+
+import co.paralleluniverse.fibers.Suspendable
+import com.github.opticyclic.corda.demo.accounts.contracts.CorpContract
+import com.github.opticyclic.corda.demo.accounts.contracts.CorpContract.Companion.CORP_CONTRACT_ID
+import com.github.opticyclic.corda.demo.accounts.states.AccountType
+import com.github.opticyclic.corda.demo.accounts.states.CorpInfo
+import com.r3.corda.lib.accounts.workflows.flows.CreateAccount
+import com.r3.corda.lib.accounts.workflows.flows.RequestKeyForAccount
+import net.corda.core.contracts.Command
+import net.corda.core.flows.FinalityFlow
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
+
+
+/**
+ * Create a custom Corda account with a property that defines the "type" of company
+ * @param companyName the company name
+ * @param companyType the [type][AccountType] of company
+ * @return [SignedTransaction]
+ * @see CorpInfo
+ */
+@StartableByRPC
+class CreateCorp(
+    private val companyName: String,
+    private val companyType: String
+) : FlowLogic<SignedTransaction>() {
+
+    /*
+     * The progress tracker checkpoints each stage of the flow and outputs the specified messages when each checkpoint is reached.
+     * See the 'progressTracker.currentStep' expressions within the call() function.
+     */
+    companion object {
+        object CREATING : ProgressTracker.Step("Creating a new output state with linked Corda Account.")
+        object BUILDING : ProgressTracker.Step("Building a new transaction.")
+        object SIGNING : ProgressTracker.Step("Signing the transaction.")
+        object FINALISING : ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+            override fun childProgressTracker() = FinalityFlow.tracker()
+        }
+
+        fun tracker() = ProgressTracker(
+            CREATING,
+            BUILDING,
+            SIGNING,
+            FINALISING
+        )
+    }
+
+    override val progressTracker = tracker()
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        progressTracker.currentStep = CREATING
+        //Create the Account to link to the output state
+        val cordaAccount = subFlow(CreateAccount(companyName))
+        val cordaAccountInfo = cordaAccount.state.data
+        //Create the output.
+        val outputState = CorpInfo(cordaAccountInfo, AccountType.valueOf(companyType))
+
+        progressTracker.currentStep = BUILDING
+        val accountKey = subFlow(RequestKeyForAccount(outputState.accountInfo)).owningKey
+        val keysToSignWith = mutableListOf(ourIdentity.owningKey, accountKey)
+        val command = Command(CorpContract.Commands.Create(), keysToSignWith)
+
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+
+        //Build the transaction.
+        val txBuilder = TransactionBuilder(notary)
+            .addOutputState(outputState, CORP_CONTRACT_ID)
+            .addCommand(command)
+
+        // Verify that the transaction is valid.
+        txBuilder.verify(serviceHub)
+
+        progressTracker.currentStep = SIGNING
+        val signedNewAccountTx = serviceHub.signInitialTransaction(txBuilder, keysToSignWith)
+
+        progressTracker.currentStep = FINALISING
+        return subFlow(FinalityFlow(signedNewAccountTx, emptyList()))
+    }
+}
